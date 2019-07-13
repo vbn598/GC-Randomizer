945a98ae0091a110901e51b147b56c54ec76f51d
Zephiles <cae222@yahoo.com>
2019-07-13 04:17:04 -0500

    Set the proper return type for createItemForTrBoxDemo

--- include/mod.h
+++ include/mod.h
@@ -15,13 +15,13 @@ namespace mod
 
 			void assemblyOverwrites();
 
-			void procCreateItemForTrBoxDemo(const float[3], s32, s32, s32, const float[3], const float[3]);
+			s32 procCreateItemForTrBoxDemo(const float[3], s32, s32, s32, const float[3], const float[3]);
 			
 		private:
 			void (*fapGm_Execute_trampoline)() = nullptr;
 			
 			bool (*checkTreasureRupeeReturn_trampoline)(void*, s32) = nullptr;
 
-			void (*createItemForTrBox_trampoline)(const float[3], s32, s32, s32, const float[3], const float[3]) = nullptr;
+			s32 (*createItemForTrBox_trampoline)(const float[3], s32, s32, s32, const float[3], const float[3]) = nullptr;
 	};
 }
--- source/mod.cpp
+++ source/mod.cpp
@@ -49,7 +49,7 @@ namespace mod
 		createItemForTrBox_trampoline = patch::hookFunction(tp::f_op_actor_mng::createItemForTrBoxDemo,
 			[](const float pos[3], s32 item, s32 unk3, s32 unk4, const float unk5[3], const float unk6[3])
 			{
-				gMod->procCreateItemForTrBoxDemo(pos, item, unk3, unk4, unk5, unk6);
+				return gMod->procCreateItemForTrBoxDemo(pos, item, unk3, unk4, unk5, unk6);
 			}
 		);
 	}
@@ -62,12 +62,12 @@ namespace mod
 		fapGm_Execute_trampoline();
 	}
 
-	void Mod::procCreateItemForTrBoxDemo(const float pos[3], s32 item, s32 unk3, s32 unk4, const float unk5[3], const float unk6[3])
+	s32 Mod::procCreateItemForTrBoxDemo(const float pos[3], s32 item, s32 unk3, s32 unk4, const float unk5[3], const float unk6[3])
 	{
 		// Runs once when Link picks up an item with text and is holding it towards the camera
 
 		// Call original function
-		createItemForTrBox_trampoline(pos, item, unk3, unk4, unk5, unk6);
+		return createItemForTrBox_trampoline(pos, item, unk3, unk4, unk5, unk6);
 	}
 	
 	void Mod::assemblyOverwrites()
